efe22450e7cf2d7be0501b025dccecdd27bbe996
Victor Watkins <vwatkinsjr@gmail.com>
2019-05-25 11:30:51 -0600

    properties file with annotations

--- src/main/java/io/vickleford/springdemo/TennisCoach.java
+++ src/main/java/io/vickleford/springdemo/TennisCoach.java
@@ -2,18 +2,29 @@ package io.vickleford.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
     private FortuneService fortuneService;
 
+    @Value("${foo.email}")
+    private String email;
+
+    @Value("${foo.team}")
+    private String team;
+
     @Autowired
     public TennisCoach(@Qualifier("randomFortuneService") FortuneService fs) {
         fortuneService = fs;
+
     }
 
     public String getDailyWorkout() {
+        System.out.println("I'm the coach for " + team +
+                " and you can email questions to " + email);
+
         return "Practice your backhand volley";
     }
 
